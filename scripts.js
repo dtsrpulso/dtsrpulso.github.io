2b46b042ebdb64d4af094fb6e6d8b5009bf19ea2
DTS Pulso Inc <dtsrpulso@gmail.com>
2025-04-08 10:47:09 -0500

    * Se agregan particulas a encabezado

--- scripts.js
+++ scripts.js
@@ -50,7 +50,7 @@ class Particle {
   constructor() {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 5 + 1;
+    this.size = Math.random() * 2 + 0.5;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
   }
